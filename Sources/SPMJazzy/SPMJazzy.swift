66dcc19a83632befef7f50ba2b8cc9abe255645a
Mark Pospesel <mark.pospesel@ymedialabs.com>
2022-05-23 22:01:44 +0200

    run swift package init

---
+++ Sources/SPMJazzy/SPMJazzy.swift
@@ -0,0 +1,11 @@
+/// SPMJazzy does it all.
+///
+/// OK, not really. It's just a test for Jazzy's documentation generation
+public struct SPMJazzy {
+    /// The text to display
+    public private(set) var text = "Hello, World!"
+
+    /// Initializes an SPMJazzy
+    public init() {
+    }
+}
---
+++ Tests/SPMJazzyTests/SPMJazzyTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SPMJazzy
+
+final class SPMJazzyTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SPMJazzy().text, "Hello, World!")
+    }
+}
